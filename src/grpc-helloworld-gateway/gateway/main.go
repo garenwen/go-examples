f14399769bd8465c46d5bff87a84eea342677a00
Andy <45716108@qq.com>
2017-04-19 09:53:49 +0800

    demo

--- src/grpc-helloworld-gateway/gateway/main.go
+++ src/grpc-helloworld-gateway/gateway/main.go
@@ -4,12 +4,13 @@ import (
 	"flag"
 	"net/http"
 
-	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
+	"fmt"
 	gw "grpc-helloworld-gateway/helloworld"
+	"io/ioutil"
 )
 
 var (
@@ -32,10 +33,25 @@ func run() error {
 }
 
 func main() {
-	flag.Parse()
-	defer glog.Flush()
+	//flag.Parse()
+	//defer glog.Flush()
+	//
+	//if err := run(); err != nil {
+	//	glog.Fatal(err)
+	//}
 
-	if err := run(); err != nil {
-		glog.Fatal(err)
-	}
+	url := "http://platform.smm.cn/mallcenter/fix/data?cmd=reload_home_data"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("cache-control", "no-cache")
+	req.Header.Add("postman-token", "3f1acf38-7c4c-6057-b6be-3cc0ba4bb2e2")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
 }
---
+++ src/protocol-buffer-http/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"protocol-buffer-http/helloworld"
+
+	"log"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	route := gin.Default()
+
+	route.Handle("GET", "/get/binary", func(c *gin.Context) {
+		test := &helloworld.Test{
+			Label: proto.String("hello"),
+			Type:  proto.Int32(17),
+			Reps:  []int64{1, 2, 3},
+			Optionalgroup: &helloworld.Test_OptionalGroup{
+				RequiredField: proto.String("good bye"),
+			},
+		}
+
+		data, err := proto.Marshal(test)
+		if err != nil {
+			log.Fatal("marshaling error: ", err)
+		}
+
+		newTest := &helloworld.Test{}
+		err = proto.Unmarshal(data, newTest)
+		if err != nil {
+			log.Fatal("unmarshaling error: ", err)
+		}
+
+		log.Println(newTest)
+
+		c.Writer.Header().Set("Content-Type", "application/octet-stream")
+
+		c.Writer.Write(data)
+
+		c.Status(http.StatusOK)
+
+	})
+
+	route.Run(":19999")
+}
